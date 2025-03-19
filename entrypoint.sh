7cfe55866bcd8918fcc7ff13b632022112917189
flickerbot <78339973+flickerbot@users.noreply.github.com>
2025-03-19 19:55:36 +0530

    Cleaned up main branch for restructuring

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Run database migrations
-echo "Running database migrations..."
-#flask db migrate
-#flask db upgrade
-
-# Start Flask application
-echo "Starting Flask application..."
-flask -A student.py run --host=${FLASK_HOST} --port=${FLASK_PORT}
